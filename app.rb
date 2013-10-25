d62cc942ae9dc5fc869993cbe9885cfcedfd29c6
Shawna C Scott <shawnatyger@gmail.com>
2013-10-24 20:31:54 -0700

    added file structure and bare page layout

--- app.rb
+++ app.rb
@@ -2,17 +2,18 @@
 require 'sinatra'
 require 'pry'
 
-
-# We us "classic" mode where you don't actually see the class
-# inheriting from Sinatra::Base and you don't start the app with
-# rackup. This makes the object structure less obvious but lets
-# Sinatra do more of the behind-the-scenes
-
 get '/' do
-  "Hello, World!"
+  erb :index
 end
 
 post '/' do
   "Caught a post" 
 end
 
+get '/thanks' do
+  erb :thanks
+end
+
+get '/suckers' do
+  erb :suckers
+end
--- test/test.rb
+++ test/test.rb
@@ -10,15 +10,42 @@ class MyTest < MiniTest::Unit::TestCase
     Sinatra::Application
   end
 
-  def test_hello_world
-    get '/'
-    assert last_response.ok?
-    assert_equal "Hello, World!", last_response.body
-  end
-
   def test_post
     post '/'
     assert last_response.ok?
     assert_equal "Caught a post", last_response.body
   end
+
+  def test_index_loads_layout
+    get '/'
+    assert last_response.ok?
+    assert last_response.body.include?("This is a layout")
+  end
+
+  def test_thanks_page_exists
+    get '/thanks'
+    assert last_response.ok?
+  end
+
+  def test_thanks_loads_layout
+    get '/thanks'
+    assert last_response.ok?
+    assert last_response.body.include?("This is a layout")
+  end
+
+  def test_suckers_page_exists
+    get '/suckers'
+    assert last_response.ok?
+  end
+
+  def test_suckers_loads_layout
+    get '/suckers'
+    assert last_response.ok?
+    assert last_response.body.include?("This is a layout")
+  end
 end
+
+
+
+
+
